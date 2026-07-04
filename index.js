e1f89e0a3887eb04f218ac85f444c2ed1adc1998
kovalgerman4-droid <kovalgerman4@gmail.com>
2026-07-04 13:30:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -752,17 +752,7 @@ function drawOrbitMap(sat, history) {
     drawTerminator(ctx, width, height, sat);
 
     // Kyiv ground station marker
-    const kyiv = project(KYIV.lon, KYIV.lat, width, height);
-    ctx.fillStyle = '#00ff88';
-    ctx.beginPath();
-    ctx.moveTo(kyiv.x, kyiv.y - 9);
-    ctx.lineTo(kyiv.x - 8, kyiv.y + 7);
-    ctx.lineTo(kyiv.x + 8, kyiv.y + 7);
-    ctx.closePath();
-    ctx.fill();
-    ctx.font = "11px 'Share Tech Mono'";
-    ctx.fillStyle = '#9debc6';
-    ctx.fillText('KYIV GS', kyiv.x + 12, kyiv.y + 3);
+ 
 
     // history ground track
     drawTrack(ctx, width, height, history, 'rgba(34,211,238,.90)', false);
